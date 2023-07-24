3bacac8b68c1020718c7acddbd69eaf86a28b1e6
ateffa24-meet <139689025+ateffa24-meet@users.noreply.github.com>
2023-07-24 10:30:25 +0300

    atef
    i am not done

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,18 +2,45 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+
+config = {
+  "apiKey": "AIzaSyBRazhO5MEcR66YfOSwIZ5oJ-4Eo2Op3tU",
+  "authDomain": "atef-f7669.firebaseapp.com",
+  "projectId": "atef-f7669",
+  "storageBucket": "atef-f7669.appspot.com",
+  "messagingSenderId": "570090090631",
+  "appId": "1:570090090631:web:e519920dab366562074822",
+  "measurementId": "G-47Z8DV47VK"
+    }
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
-    return render_template("signin.html")
+    if request.methods=='post':
+        email=request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.creat_user_with_email_and_password(email, password)
+        except:
+            return render_template("signin.html")
+
+    
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    error=""
+    if request.methods== 'POST':
+        email=request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.creat_user_with_email_and_password(email, password)
+        except:
+            return render_template("signup.html")
+
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
